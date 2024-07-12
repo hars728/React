f1d82891b55c7a49d87ae0fc68431d7548d89adf
HARSH KUMAR GUPTA <133455584+hars728@users.noreply.github.com>
2024-07-12 19:02:34 +0530

    bootstrap

---
+++ learnbootstr/src/Hello.jsx
@@ -0,0 +1,4 @@
+function Hello() {
+    return <h1>this is goood one </h1>
+}
+export default Hello;
---
+++ learnbootstr/src/Random.jsx
@@ -0,0 +1,6 @@
+function Random() {
+    let no = Math.random() * 100;
+
+    return <h1>Ramdom no is{Math.floor(no)}</h1>
+}
+export default Random;
