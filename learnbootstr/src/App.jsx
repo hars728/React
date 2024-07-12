eb15bdf6276a0f5480d6274791b2df8113f7bdec
HARSH KUMAR GUPTA <133455584+hars728@users.noreply.github.com>
2024-07-12 19:29:20 +0530

    bootstrap

--- learnbootstr/src/App.jsx
+++ learnbootstr/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
     <Random ></Random>
     <Random ></Random>
     <Hello></Hello>
+
     <button type="button" class="btn btn-primary">Primary</button>
     <button type="button" class="btn btn-secondary">Secondary</button>
     <button type="button" class="btn btn-success">Success</button>
